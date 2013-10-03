a083bb713651ad23baeb4abc8ae2dec3bd345b0e
Qasim Zaidi <qasim@zaidi.me>
2013-10-03 10:46:53 +0530

    Add javascript API.

---
+++ js/quran.js
@@ -0,0 +1,28 @@
+function toArabDigits(num) {
+  var anum = '';
+  var arabdigits = [ '٠', '١', '٢', '٣', '٤', '٥', '٦', '٧',  '٨','٩' ];
+  num = Number(num);
+  while (num) {
+    anum += arabdigits[num%10]; 
+    num = Math.floor(num/10,0);
+  }
+  return anum;
+}
+
+var thisScript = document.currentScript || document.getElementsByTagName('script').pop();
+var chapter = thisScript.getAttribute('chapter')|0;
+var verse = thisScript.getAttribute('verse')|0;
+var selector = thisScript.getAttribute('selector');
+var renderFunc = function(x) { 
+  document.querySelector(selector).innerText = x.entry.content.$t + ' ﴿' + toArabDigits(verse) + '﴾ ';
+};
+var vnum = 0;
+for (var i = 1; i < chapter; i++) {
+  vnum += QuranData.Sura[i][1];
+}
+vnum += verse + 1; // compensate for header row
+var gs = document.createElement('script');
+gs.async="true";
+gs.src = "https://spreadsheets.google.com/feeds/cells/0Aps7j0tW_eq0dFUzN3djMC1IUUYyMHV4VFhqRUhJSmc/od6/public/values/R" + vnum + "C3?alt=json-in-script&callback=renderFunc";
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(gs, s);
